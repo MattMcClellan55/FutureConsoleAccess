8e8a04451792cc486f27462204e658a3bd9c1de3
Matt McClellan <Matt McClellan@DESKTOP-META>
2026-08-24 23:32:06 -0400

    initial development of node system

--- FutureConsoleAccess/Program.cs
+++ FutureConsoleAccess/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Earth!");
+﻿List<Node> nodes = new List<Node>();
+Console.WriteLine("What are you thinking of right now?");
+string? thought = Console.ReadLine();
+if (thought is not null) nodes.Add(new Node (0, 0, thought));
+
+foreach (Node node in nodes) 
+    Console.WriteLine("Interesting, you're thinking of " + node.thought);
+
+public struct Node
+{
+    public int id;
+    public int tier;
+    public string thought;
+
+    public Node(int id, int tier, string thought)
+    {
+        this.id = id;
+        this.tier = tier;
+        this.thought = thought;
+    }
+}
