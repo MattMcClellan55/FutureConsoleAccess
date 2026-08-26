749c82c83a6b74a5e2ce125afa4cfc069b520a63
Matt McClellan <Matt McClellan@DESKTOP-META>
2026-08-25 21:32:37 -0400

    switched to dictionary format and added more node layering options

--- FutureConsoleAccess/Program.cs
+++ FutureConsoleAccess/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
-int index = 0;
-List<Node> nodes = new List<Node>();
+Dictionary<string, Node> nodes = new Dictionary<string, Node>();
 Console.WriteLine("What are you thinking of right now?");
 string? thought = Console.ReadLine();
-if (thought is not null) nodes.Add(new Node (thought));
 
-while (!(string.Equals(thought, "close", StringComparison.OrdinalIgnoreCase) ||
-         string.Equals(thought, "end", StringComparison.OrdinalIgnoreCase) ||
-         string.Equals(thought, "quit", StringComparison.OrdinalIgnoreCase) ||
-         string.Equals(thought, "stop", StringComparison.OrdinalIgnoreCase)))
+if (string.IsNullOrEmpty(thought)) thought = "[]";
+string index = thought;
+nodes.Add(index, new Node(thought));
+
+do
 {
     Console.WriteLine("Anything else on your mind?");
     thought = Console.ReadLine();
     if (string.IsNullOrEmpty(thought)) break;
+    thought = thought.Replace('\'', '\"');
 
     string[] command = Regex.Matches(thought, @"""[^""]*""|\w+").Cast<Match>()
         .Select(m => m.Value).ToArray();
@@ -23,6 +24,22 @@ while (!(string.Equals(thought, "close", StringComparison.OrdinalIgnoreCase) ||
         Console.WriteLine("Please enter a valid command.");
         continue;
     }
+    else if (string.Equals(command[0], "adopt", StringComparison.OrdinalIgnoreCase))
+    {
+        if (command.Length != 3)
+        {
+            Console.WriteLine("Please enter a valid command.");
+            continue;
+        }
+        else if (command[1][0] != '\"' || !nodes.ContainsKey(command[1].Trim('\"')) || 
+            command[2][0] != '\"' || !nodes.ContainsKey(command[2].Trim('\"')))
+        {
+            Console.WriteLine("Please enter valid indexes.");
+            continue;
+        }
+
+        ParentChildLink(nodes[command[1].Trim('\"')], nodes[command[2].Trim('\"')]);
+    }
     else if (string.Equals(command[0], "birth", StringComparison.OrdinalIgnoreCase))
     {
         if (command.Length > 3 || command[command.Length - 1][0] != '\"')
@@ -30,40 +47,97 @@ while (!(string.Equals(thought, "close", StringComparison.OrdinalIgnoreCase) ||
             Console.WriteLine("Please enter a valid command.");
             continue;
         }
+        else if (nodes.ContainsKey(command[command.Length - 1].Trim('\"')))
+        {
+            Console.WriteLine("This entry already exists.");
+            continue;
+        }
         else if (command.Length == 3)
         {
-            if (Int32.TryParse(command[1], out int newIndex) && newIndex < nodes.Count) index = newIndex;
-            else
+            if (command[1][0] != '\"' || !nodes.ContainsKey(command[1].Trim('\"')))
             {
                 Console.WriteLine("Please enter a valid index.");
                 continue;
             }
+            index = command[1].Trim('\"');
         }
 
-        nodes.Add(new Node(command[command.Length - 1].Trim('\"')));
-        ParentChildLink(nodes[index], nodes[nodes.Count - 1]);
-        index = nodes.Count - 1;
+        nodes.Add(command[command.Length - 1].Trim('\"'), new Node(command[command.Length - 1].Trim('\"')));
+        ParentChildLink(nodes[index], nodes[command[command.Length - 1].Trim('\"')]);
+    }
+    else if (string.Equals(command[0], "create", StringComparison.OrdinalIgnoreCase))
+    {
+        if (command.Length > 2 || command[command.Length - 1][0] != '\"')
+        {
+            Console.WriteLine("Please enter a valid command.");
+            continue;
+        }
+        else if (nodes.ContainsKey(command[command.Length - 1].Trim('\"')))
+        {
+            Console.WriteLine("This entry already exists.");
+            continue;
+        }
+
+        nodes.Add(command[command.Length - 1].Trim('\"'), new Node(command[command.Length - 1].Trim('\"')));
+        index = command[command.Length - 1].Trim('\"');
+    }
+    else if (string.Equals(command[0], "proxy", StringComparison.OrdinalIgnoreCase))
+    {
+        if (command.Length != 4)
+        {
+            Console.WriteLine("Please enter a valid command.");
+            continue;
+        }
+        else if (command[1][0] != '\"' || !nodes.ContainsKey(command[1].Trim('\"')) ||
+            command[2][0] != '\"' || !nodes.ContainsKey(command[2].Trim('\"')) || 
+            command[3][0] != '\"' || nodes.ContainsKey(command[3].Trim('\"')))
+        {
+            Console.WriteLine("Please enter valid indexes.");
+            continue;
+        }
+        else if (!nodes[command[1].Trim('\"')].child.Any(c => c.text == command[2].Trim('\"')))
+        {
+            Console.WriteLine("There is no in-between to proxy from here.");
+            continue;
+        }
+
+        nodes[command[1].Trim('\"')].child.RemoveAll(p => p.text == command[2].Trim('\"'));
+        nodes[command[2].Trim('\"')].parent.RemoveAll(p => p.text == command[1].Trim('\"'));
+
+        nodes.Add(command[3].Trim('\"'), new Node(command[3].Trim('\"')));
+        index = command[3].Trim('\"');
+
+        ParentChildLink(nodes[command[1].Trim('\"')], nodes[command[3].Trim('\"')]);
+        ParentChildLink(nodes[command[3].Trim('\"')], nodes[command[2].Trim('\"')]);
     }
     else if (string.Equals(command[0], "remove", StringComparison.OrdinalIgnoreCase))
     {
-        if (Int32.TryParse(command[1], out int removeIndex) && removeIndex < nodes.Count)
+        if (command.Length > 2 || command[1][0] != '\"')
         {
-            nodes.RemoveAt(removeIndex);
-            if (nodes.Count == 0) nodes.Add(new Node("[]"));
-            if (index >= nodes.Count) index = nodes.Count - 1;
+            Console.WriteLine("Please enter a valid command.");
+            continue;
         }
-        else
+        else if (!nodes.ContainsKey(command[1].Trim('\"')))
         {
             Console.WriteLine("Please enter a valid index.");
             continue;
         }
+
+        nodes.Remove(command[1].Trim('\"'));
+        if (nodes.Count == 0) nodes.Add("[]", new Node("[]"));
+        if (!nodes.ContainsKey(index)) index = nodes.Keys.First();
+
+        foreach (Node node in nodes.Values.ToList())
+        {
+            node.parent.RemoveAll(p => p.text == command[1].Trim('\"'));
+            node.child.RemoveAll(c => c.text == command[1].Trim('\"'));
+        }
     }
     else if (string.Equals(command[0], "summarize", StringComparison.OrdinalIgnoreCase))
     {
-        foreach (Node node in nodes)
+        foreach (Node node in nodes.Values.ToList())
         {
-            // if (node.parent.Count > 0) continue;
-            if (node.child.Count == 0) Console.WriteLine(node.text);
+            if (node.parent.Count == 0 && node.child.Count == 0) Console.WriteLine(node.text);
 
             foreach (Node child in node.child)
             {
@@ -72,6 +146,10 @@ while (!(string.Equals(thought, "close", StringComparison.OrdinalIgnoreCase) ||
         }
     }
 }
+while (!(string.Equals(thought, "close", StringComparison.OrdinalIgnoreCase) ||
+         string.Equals(thought, "end", StringComparison.OrdinalIgnoreCase) ||
+         string.Equals(thought, "quit", StringComparison.OrdinalIgnoreCase) ||
+         string.Equals(thought, "stop", StringComparison.OrdinalIgnoreCase)));
 
 static void ParentChildLink(Node parent, Node child)
 {
@@ -83,12 +161,10 @@ public struct Node
 {
     public List<Node> parent; 
     public List<Node> child;
-    public int tier;
     public string text;
 
     public Node(string text)
     {
-        this.tier = 0;
         this.text = text;
         this.parent = new List<Node>();
         this.child = new List<Node>();
